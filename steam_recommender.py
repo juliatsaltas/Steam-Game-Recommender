c6e25f9c578730cc4dacb8b9cbc93e9ad2c4033c
Julia T <49352190+juliatsaltas@users.noreply.github.com>
2021-09-09 21:06:49 -0400

    Update

--- steam_recommender.py
+++ steam_recommender.py
@@ -2,38 +2,41 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+import timeit
+
+ 
+
 def app():
 	st.write("""
 	# Steam Game Recommender
+	- Data obtained from the Steam Store raw dataset from [Kaggle](https://www.kaggle.com/tamber/steam-video-games) by Tamber.com.
+	- Data cleaning and EDA presented here: [link](https://www.kaggle.com/fivewheeler/steam-video-games-eda)
 
-	Welcome! Can't decide what game to play next? Have a favourite game and want to know what other users who've also played your favourite are playing?
+	### Welcome! 
 
-	Data obtained from the Steam Store raw dataset from [Kaggle](https://www.kaggle.com/tamber/steam-video-games) by Tamber.com.
-	Data cleaning and EDA presented here: [link](https://www.kaggle.com/fivewheeler/steam-video-games-eda)
+	Can't decide what game to play next? Have a favourite game and want to know what other users who've also played your favourite are playing?
+
 	
-	This site can take up to half a minute to load: 
-
 	""")
+	start = timeit.default_timer()
+
+	# 1. Setup cache function - brought down data load 
+	# runtime from 35 to 0.003 seconds
 	@st.cache(allow_output_mutation=True)
 	def load_data(file_name):
 		data_file = pd.read_csv(file_name)
 		return data_file
-
+	
+	# 2. Upload dataframe with clustered user data
 	steam = load_data('steam_clean_recommender.csv')
 	pivot = load_data('steam_recommender_pivot.csv')
-
-	### 1. Upload dataframe with clustered user data
-	#steam = pd.read_csv('steam_clean_recommender.csv')
-	#pivot = pd.read_csv('steam_recommender_pivot.csv')
-	pivot.set_index('game',inplace=True)
-	df_recommender = pd.read_csv('steam_recommender.csv')
-	df_recommender.set_index('game',inplace=True)
+	df_recommender = load_data('steam_recommender.csv')
 
 	st.write("""
 	Input a game title or part of a game title to get a list of games other users have purchased together.
 	""")
 
-	# 2. User input of game
+	# 3. User input of game
 	def user_input_features():
 		search = st.text_input("Search Game Title")
 		data = {'search': search}
@@ -42,13 +45,17 @@ def app():
 	
 	user_input = user_input_features()
 	
-	# 3. save user_input and convert to lower case to make case insensitive
+	# 4. save user_input and convert to lower case to make case insensitive
 	search = user_input['search'][0]
 	steam['game_lower'] = [i.lower() for i in steam['game']]
 	
+	stop = timeit.default_timer()
+	st.write('Search Runtime: ', stop - start)
+
+	# 5. print tables
 	count = 0
 	for title in steam.loc[steam['game_lower'].str.contains(search.lower()), 'game']:
-		st.write(title)
+		st.write(f"### {title}")
 		st.write(f"Number of Players: {pivot.T[title].count()}")
 		st.write("10 closest titles:")
 		st.write(round(df_recommender[title].sort_values(ascending=False)[1:11], 2))
